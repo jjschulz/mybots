2b0625f6106f442770617fd85762670259012229
jjschulz <johnna.schulz@uvm.edu>
2025-03-08 15:39:58 -0500

    randomsearch

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 import pyrosim.pyrosim as pyrosim
+import random
 
 def Create_Robot():
     pass
@@ -27,7 +28,13 @@ def Generate_Brain():
     pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "Frontleg")
     pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_Backleg")
     pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_Frontleg")
-
+    # pyrosim.Send_Synapse(sourceNeuronName = 1 , targetNeuronName = 3 , weight = 0.1)
+    # pyrosim.Send_Synapse(sourceNeuronName = 2 , targetNeuronName = 3 , weight = 1.1)
+    # pyrosim.Send_Synapse(sourceNeuronName = 2 , targetNeuronName = 4 , weight = 2.0)
+    # pyrosim.Send_Synapse(sourceNeuronName = 1 , targetNeuronName = 4 , weight = 0.1)
+    for i in range(3):
+        for j in range(3,5):
+            pyrosim.Send_Synapse(sourceNeuronName=i, targetNeuronName=j,weight=2*random.random()-1)
     pyrosim.End()
 # def Create_Robot():
 #     pyrosim.Start_URDF("body.urdf")
--- pyrosim/neuron.py
+++ pyrosim/neuron.py
@@ -47,8 +47,23 @@ class NEURON:
     def Update_Sensor_Neuron(self):
         self.Set_Value(pyrosim.Get_Touch_Sensor_Value_For_Link(self.Get_Link_Name()))
 
-    def Update_Hidden_Or_Motor_Neuron(self):
-        self.Set_Value(math.pi/4)
+    def Update_Hidden_Or_Motor_Neuron(self, neurons, synapses):
+        self.Set_Value(0.0)
+        print(self.Get_Name())
+        for key in synapses:
+            print(key)
+            if key[1] == self.Get_Name():
+                weight=synapses[key].Get_Weight()
+                name=key[0]
+                print('original neruon value: ', self.Get_Value())
+                self.Allow_Presynaptic_Neuron_To_Influence_Me(weight, neurons[name].Get_Value())
+                print('new neuron value: ', self.Get_Value())
+                print('weight: ', weight, 'value: ',neurons[name].Get_Value())
+        self.Threshold()
+
+    def Allow_Presynaptic_Neuron_To_Influence_Me(self,weight, name):
+        result=weight*name
+        self.Add_To_Value(result)
 
     def Is_Hidden_Neuron(self):
 
