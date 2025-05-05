7cefd7a535f012148e7067ae97dddd8af719e29a
jjschulz <johnna.schulz@uvm.edu>
2025-05-05 11:47:38 -0400

    plot fitness curve

--- plotFitnessValues.py
+++ plotFitnessValues.py
@@ -1,19 +1,22 @@
 import numpy as np
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 
-fitness_B_matrix = np.load('fitness_B_matrix.npy')
-fitness_A_matrix = np.load('fitness_A_matrix.npy')
+a_matrices = [np.load(f'data/fitness_A_matrix{i}.npy') for i in range(10)]
+avg_fitness_A_matrix = np.mean(a_matrices, axis=0)
+avg_fitness_A = np.mean(avg_fitness_A_matrix, axis=0)
 
-avg_fitness_A = np.mean(fitness_A_matrix, axis=0)
-avg_fitness_B = np.mean(fitness_B_matrix, axis=0)
+#load and average a and b matrices
+b_matrices = [np.load(f'data/fitness_B_matrix{i}.npy') for i in range(10)]
+avg_fitness_B_matrix = np.mean(b_matrices, axis=0)
+avg_fitness_B = np.mean(avg_fitness_B_matrix, axis=0)
 
-matplotlib.pyplot.plot(avg_fitness_A, label='A AVG')
-matplotlib.pyplot.plot(avg_fitness_B, label = 'B AVG')
+#plot
+plt.plot(avg_fitness_A, label='A AVG')
+plt.plot(avg_fitness_B, label='B AVG')
 
-matplotlib.pyplot.xlabel('Generation')
-matplotlib.pyplot.ylabel('Average Fitness')
-matplotlib.pyplot.title('Average Fitness per Generation (A vs. B)')
-matplotlib.pyplot.legend()
-matplotlib.pyplot.grid(True)
-
-matplotlib.pyplot.show()
+plt.xlabel('Generation')
+plt.ylabel('Average Fitness')
+plt.title('Average Fitness as generation evolves (A vs. B)')
+plt.legend()
+plt.grid(True)
+plt.show()
